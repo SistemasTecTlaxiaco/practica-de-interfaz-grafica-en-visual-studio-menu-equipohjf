3bb7031a1a0c642618fda201f351b8508a378ac5
Federico <federicovictorianosierra@gmail.com>
2022-02-15 12:17:33 -0600

    Hice cambios

--- Equipo-HJF/Formulario1.Designer.cs
+++ Equipo-HJF/Formulario1.Designer.cs
@@ -30,6 +30,7 @@ namespace Equipo_HJF
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -41,22 +42,35 @@ namespace Equipo_HJF
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(233, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Si se Pudo??";
+            // 
+            // Formulario1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Form1";
+            this.Name = "Formulario1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
