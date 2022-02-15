4a22e503e3f2c0c02aec703d56259bcef67239bf
José Antonio Balbuena <JoseABalbuena2@hotmail.com>
2022-02-15 16:25:52 -0600

    Avance 2

--- Equipo-HJF/Form2.Designer.cs
+++ Equipo-HJF/Form2.Designer.cs
@@ -68,6 +68,7 @@ namespace Equipo_HJF
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(1263, 7);
             this.pictureBox1.Name = "pictureBox1";
@@ -75,6 +76,7 @@ namespace Equipo_HJF
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form2
             // 
