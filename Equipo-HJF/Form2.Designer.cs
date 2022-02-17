324ad5d188e03a5a3799ec6490d42d30f1fa382b
HEIDI LUCY BAUTISTA SANJUAN <heidi_lbs@tlaxiaco.tecnm.mx>
2022-02-16 20:16:12 -0600

    Cambios color2

--- Equipo-HJF/Form2.Designer.cs
+++ Equipo-HJF/Form2.Designer.cs
@@ -116,7 +116,7 @@ namespace Equipo_HJF
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
             this.label1.Location = new System.Drawing.Point(141, 1);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(117, 25);
